cc13e06df39d7d10ffbde1e642f46116d5980d46
EC2 Default User <miyaichi@gmail.com>
2024-02-27 07:02:20 +0000

    add upload_file_to_s3

--- handler.py
+++ handler.py
@@ -1,7 +1,10 @@
 import base64
 import boto3
+import cgi
+import datetime
 import hashlib
 import io
+import json
 import logging
 import os
 
@@ -9,6 +12,45 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3')
+
+
+# Store uploaded files in S3
+def upload_file_to_s3(event, context):
+    logger.info("New file uploaded.")
+
+    # Generate file name in YYYYMMDD-HHMMSS.csv format using the current time.
+    key = datetime.datetime.now(
+        datetime.timezone(datetime.timedelta(hours=9),
+                          'JST')).strftime("%Y%m%d-%H%M%S.csv")
+
+    # Parse multi-part data
+    _, c_data = cgi.parse_header(event['headers']['content-type'])
+    c_data['boundary'] = bytes(c_data['boundary'], 'utf8')
+    form_data = cgi.parse_multipart(io.BytesIO(bytes(event['body'], 'utf8')),
+                                    c_data)
+
+    # Store in S3
+    s3.put_object(Bucket=os.environ['source_bucket'],
+                  Key=key,
+                  Body=form_data['file'][0])
+
+    location = s3.generate_presigned_url(ClientMethod='get_object',
+                                         Params={
+                                             'Bucket':
+                                             os.environ['destination_bucket'],
+                                             'Key':
+                                             key
+                                         },
+                                         ExpiresIn=3600,
+                                         HttpMethod='GET')
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "uploaded": "true",
+            "location": location
+        })
+    }
 
 
 # Normalize and encoding email addresses in S3 bucket files
@@ -26,23 +68,25 @@ def normalization_and_encoding(event, context):
     for line in source['Body']._raw_stream:
         data_str = line.decode('utf-8').rstrip()
         if len(data_str):
-            email = encoded = ''
+            encoded = ''
             if is_email(data_str):
-                email = normalize_email_string(data_str)
-                encoded = base64_encode(hash_sha256(email))
-
-            buffer.write("{},{},{}\n".format(data_str, email, encoded))
+                encoded = base64_encode(
+                    hash_sha256(normalize_email_string(data_str)))
+                buffer.write("{}\n".format(encoded))
 
     # Write to destination bucket
     buffer.seek(0)
     s3.put_object(Bucket=destination_bucket, Key=key, Body=buffer.read())
+
+    # Delete source file
+    s3.delete_object(Bucket=source_bucket, Key=key)
 
 
 # Determine if the string is a email address
 def is_email(data):
     if data.count('@') != 1:
         return False
-    local, domain = data.split('@')
+    _, domain = data.split('@')
     if domain.count('.') != 1:
         return False
     return True
