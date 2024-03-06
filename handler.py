c47df28723bcf214f4baa5bb7839930987158a34
Yoshihiko Miyaichi <miyaichi@gmail.com>
2024-03-06 14:51:56 +0900

    Code cleanup.

--- handler.py
+++ handler.py
@@ -1,16 +1,17 @@
+""" Lambda function for the File Upload to S3 and Data Normalization and Encoding. """
 import base64
-import boto3
 import cgi
 import datetime
 import hashlib
 import io
-import jinja2
-import json
 import logging
 import os
-import phonenumbers
 import random
 import uuid
+
+import boto3
+import jinja2
+import phonenumbers
 from botocore.config import Config
 
 # -*- coding: utf-8 -*-
@@ -19,13 +20,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def upload_file_to_s3(event: dict, context: dict) -> dict:
+def upload_file_to_s3(event: dict, _context: dict) -> dict:
     """
     Uploads a file to S3 and returns an HTML form for file upload.
 
     Args:
         event (dict): The event data.
-        context (dict): The context data.
+        _context (dict): The context data.
 
     Returns:
         dict: The response containing the HTML form or an error message.
@@ -33,19 +34,17 @@ def upload_file_to_s3(event: dict, context: dict) -> dict:
     logger.info("File upload request.")
 
     # Initialize S3 and Jinja2 environment.
-    s3 = boto3.client('s3',
-                      config=Config(region_name=os.environ["AWS_REGION"],
-                                    signature_version="s3v4"))
+    s3_client = boto3.client('s3',
+                             config=Config(
+                                 region_name=os.environ["AWS_REGION"],
+                                 signature_version="s3v4"))
     environment = jinja2.Environment(loader=jinja2.FileSystemLoader(
         searchpath="./templates"))
 
     # Get region code parameter.
     region_code = os.environ["region_code"]
-    try:
-        region_code = event.get("queryStringParameters",
-                                {}).get("region_code", region_code)
-    except:
-        pass
+    region_code = event.get("queryStringParameters",
+                            {}).get("region_code", region_code)
 
     if event["httpMethod"] == "GET":
         try:
@@ -67,10 +66,12 @@ def upload_file_to_s3(event: dict, context: dict) -> dict:
                     "path": event["requestContext"]["resourcePath"]
                 })
             }
-        except:
+        except Exception as error:
             return {
-                "statusCode": 500,
-                "body": "An error occurred while rendering the template."
+                "statusCode":
+                500,
+                "body":
+                f"An error occurred while rendering the template: {str(error)}"
             }
 
     if event["httpMethod"] == "POST":
@@ -87,22 +88,24 @@ def upload_file_to_s3(event: dict, context: dict) -> dict:
 
             # Get data_type parameter.
             data_type = form_data.get("data_type", [data_type])[0]
-        except:
+        except Exception as error:
             return {
-                "statusCode": 400,
-                "body": "An error occurred while parsing the multi-part data."
+                "statusCode":
+                400,
+                "body":
+                f"An error occurred while parsing the multi-part data: {str(error)}"
             }
 
         try:
             # Store in S3.
             header = data_type.encode() + b"\n"
             data = header + form_data["file"][0]
-            s3.put_object(Bucket=os.environ["source_bucket"],
-                          Key=key,
-                          Body=data)
+            s3_client.put_object(Bucket=os.environ["source_bucket"],
+                                 Key=key,
+                                 Body=data)
 
             # Generate a pre-signed URL.
-            location = s3.generate_presigned_url(
+            location = s3_client.generate_presigned_url(
                 ClientMethod="get_object",
                 Params={
                     "Bucket": os.environ["destination_bucket"],
@@ -126,20 +129,24 @@ def upload_file_to_s3(event: dict, context: dict) -> dict:
                     "expires_in": os.environ["expires_in"]
                 })
             }
-        except:
+        except Exception as error:
             return {
-                "statusCode": 500,
-                "body": "An error occurred while storing the file in S3."
+                "statusCode":
+                500,
+                "body":
+                f"An error occurred while storing the file in S3: {str(error)}"
             }
 
+    return {"statusCode": 400, "body": "Invalid request."}
 
-def clean_up_buckets(event: dict, context: dict) -> dict:
+
+def clean_up_buckets(_event: dict, _context: dict) -> dict:
     """
     Cleans up the source and destination buckets by deleting expired objects.
 
     Args:
-        event (dict): The event data.
-        context (dict): The context data.
+        _event (dict): The event data.
+        _context (dict): The context data.
 
     Returns:
         dict: The response indicating the success or failure of the cleanup operation.
@@ -147,9 +154,10 @@ def clean_up_buckets(event: dict, context: dict) -> dict:
     logger.info("Clean up source and destination bucket.")
 
     # Initialize S3.
-    s3 = boto3.client('s3',
-                      config=Config(region_name=os.environ["AWS_REGION"],
-                                    signature_version="s3v4"))
+    s3_client = boto3.client('s3',
+                             config=Config(
+                                 region_name=os.environ["AWS_REGION"],
+                                 signature_version="s3v4"))
 
     # Expires_in minutes before current time.
     date = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(
@@ -158,7 +166,7 @@ def clean_up_buckets(event: dict, context: dict) -> dict:
     # Scan buckets and delete old objects.
     buckets = [os.environ["source_bucket"], os.environ["destination_bucket"]]
     for bucket in buckets:
-        response = s3.list_objects_v2(Bucket=bucket)
+        response = s3_client.list_objects_v2(Bucket=bucket)
         if "Contents" in response:
             for obj in response["Contents"]:
                 last_modified = obj["LastModified"]
@@ -166,43 +174,44 @@ def clean_up_buckets(event: dict, context: dict) -> dict:
                 if last_modified < date:
                     try:
                         # Delete expired object.
-                        s3.delete_object(Bucket=bucket, Key=key)
+                        s3_client.delete_object(Bucket=bucket, Key=key)
                         logger.info(
-                            f"Deleted Key {key} in Bucket {bucket} (last_modified: {last_modified})."
-                        )
-                    except:
+                            "Deleted Key %s in Bucket %s (last_modified: %s).",
+                            key, bucket, last_modified)
+                    except Exception as error:
                         logger.error(
-                            f"An error occurred while deleting Key {key} in Bucket {bucket}."
-                        )
+                            f"An error occurred while deleting Key %s in Bucket %s: {str(error)}",
+                            key, bucket)
 
     return {"statusCode": 200}
 
 
-def normalization_and_encoding(event: dict, context: dict) -> dict:
+def normalization_and_encoding(event: dict, _context: dict) -> dict:
     """
     Normalizes and encodes email addresses in S3 bucket files.
 
     Args:
         event (dict): The event data.
-        context (dict): The context data.
+        _context (dict): The context data.
 
     Returns:
-        dict: The response indicating the success or failure of the normalization and encoding process.
+        dict: The response indicating the success or failure of the normalization and encoding.
     """
     logger.info("New files uploaded to the source bucket.")
 
     # Initialize S3.
-    s3 = boto3.client('s3',
-                      config=Config(region_name=os.environ["AWS_REGION"],
-                                    signature_version="s3v4"))
+    s3_client = boto3.client('s3',
+                             config=Config(
+                                 region_name=os.environ["AWS_REGION"],
+                                 signature_version="s3v4"))
 
     try:
         source_bucket = event["Records"][0]["s3"]["bucket"]["name"]
         key = event["Records"][0]["s3"]["object"]["key"]
-        logger.info(f"source: {source_bucket}, key: {key}")
+        logger.info("source: %s, key: %s", source_bucket, key)
 
         # Process the file.
-        source = s3.get_object(Bucket=source_bucket, Key=key)
+        source = s3_client.get_object(Bucket=source_bucket, Key=key)
         region_code = os.environ["region_code"].upper()
         data_type = "email"
         encoded_list = []
@@ -229,25 +238,32 @@ def normalization_and_encoding(event: dict, context: dict) -> dict:
                     base64_encode(
                         hash_sha256(
                             normalize_phone_number(data_str, region_code))))
-    except:
-        logger.error("An error occurred while processing the file.")
-        return {"statusCode": 500}
+    except Exception as error:
+        return {
+            "statusCode": 500,
+            "body":
+            f"An error occurred while processing the file: {str(error)}"
+        }
 
     try:
         destination_bucket = os.environ["destination_bucket"]
-        logger.info(f"destination: {destination_bucket}, key: {key}")
+        logger.info("destination: %s, key: %s", destination_bucket, key)
 
         # Write to destination bucket.
         buffer = io.TextIOWrapper(io.BytesIO(), encoding="utf-8")
         for encoded in random_sort(encoded_list):
             buffer.write(f"{encoded}\n")
         buffer.seek(0)
-        s3.put_object(Bucket=destination_bucket, Key=key, Body=buffer.read())
-    except:
-        logger.error(
-            "An error occurred while storing the file in the destination bucket."
-        )
-        return {"statusCode": 500}
+        s3_client.put_object(Bucket=destination_bucket,
+                             Key=key,
+                             Body=buffer.read())
+    except Exception as error:
+        return {
+            "statusCode":
+            500,
+            "body":
+            f"An error occurred while storing the file in the destination bucket: {str(error)}"
+        }
 
     return {"statusCode": 200}
 
@@ -309,7 +325,7 @@ def is_phone_number(phone: str, region_code: str) -> bool:
     try:
         phonenumbers.parse(phone, region_code)
         return True
-    except:
+    except phonenumbers.phonenumberutil.NumberParseException:
         return False
 
 
@@ -328,22 +344,22 @@ def normalize_phone_number(phone: str, region_code: str) -> str:
         phone = phonenumbers.format_number(
             phonenumbers.parse(phone, region_code),
             phonenumbers.PhoneNumberFormat.E164)
-    except:
+    except phonenumbers.phonenumberutil.NumberParseException:
         phone = ""
     return phone
 
 
-def base64_encode(b: bytes) -> str:
+def base64_encode(data: bytes) -> str:
     """
     Encodes bytes using Base64.
 
     Args:
-        b (bytes): The bytes to encode.
+        data (bytes): The bytes to encode.
 
     Returns:
         str: The Base64 encoded string.
     """
-    return base64.b64encode(b).decode()
+    return base64.b64encode(data).decode()
 
 
 def hash_sha256(data: str) -> bytes:
@@ -369,7 +385,7 @@ def random_sort(lst: list) -> list:
     Returns:
         list: The randomly sorted list.
     """
-    for i in range(len(lst)):
+    for i, _ in enumerate(lst):
         j = random.randint(0, i)
         lst[i], lst[j] = lst[j], lst[i]
     return lst
